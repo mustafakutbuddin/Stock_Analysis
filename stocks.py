258d1fdccc4133b0003f5f81dbf306b57fd66ba5
mustafakutbuddin <mustafakutbuddin2002@gmail.com>
2024-02-14 19:09:30 +0530

    Refined Stock Class

--- stocks.py
+++ stocks.py
@@ -13,53 +13,80 @@ class Stocks:
         self.hist_data = yf.download(
             tickers=symbol, interval='1d', period='1y', rounding=True)  # YYYY-MM-DD
 
-        self.open_month = self.hist_data.loc[:, 'Open']
-        self.close_month = self.hist_data.loc[:, 'Adj Close']
-
-        # Get the current date
-        self.todays_date = datetime.now().date()
-
         print('-'*50)
         print(self.hist_data.head())
         print('-'*50)
 
+        # Get the current date
+        self.todays_date = datetime.now().date()
+
     def CurPrice(self, date):
-        # self.cur_close = self.close_month[-1:]
-        return self.hist_data.loc[str(date)]['Adj Close']
+        return (self.hist_data.loc[str(date)]['Adj Close'])
 
     def MonthlyRet(self, start_date=None):
         if start_date is None:
+            # print('in if statement of monthly ret')
+
             start_date = self.todays_date
             prevMonthDate = self.todays_date - timedelta(days=30)
             prices = self.hist_data.loc[prevMonthDate:start_date]['Adj Close']
+            monthlyret = (
+                (prices.iloc[-1] - prices.iloc[0]) / prices.iloc[0]) * 100
 
-            return (((prices.iloc[-1] - prices.iloc[0]) / prices.iloc[0]) * 100)
+            return (round(monthlyret, 2))
 
         else:
+            # print('in else statement of monthly ret')
+
             start_date = datetime.strptime(start_date, "%Y-%m-%d")
             prevMonthDate = start_date - timedelta(days=30)
             prices = self.hist_data.loc[prevMonthDate:start_date]['Adj Close']
+            monthlyret = (
+                (prices.iloc[-1] - prices.iloc[0]) / prices.iloc[0]) * 100
 
-            return (((prices.iloc[-1] - prices.iloc[0]) / prices.iloc[0]) * 100)
+            return (round(monthlyret, 2))
 
-    def DailyReturn(self):
-        # self.d_return = ((self.close_month.iloc[-1]/self.close_month.iloc[-2])-1)
-        # return (self.curPrice(self.todays_date) - self.curPrice(prevDate)) / self.curPrice(prevDate)
-        prevDate = self.todays_date - timedelta(days=1)
-        return ((self.CurPrice(self.todays_date)/self.CurPrice(prevDate))-1)
+    def DailyReturn(self, date=None):
+        if date is None:
+            # print('in if statement of daily ret')
 
-    def Last30Dayprice(self):
-        # self.last30dayprice = self.close_full[-30:]
-        prevMonthDate = self.todays_date - timedelta(days=30)
-        return (self.hist_data.loc[prevMonthDate:self.todays_date]['Adj Close']).to_numpy()
+            prevDate = self.todays_date - timedelta(days=1)
+            dailyret = (self.CurPrice(self.todays_date) /
+                        self.CurPrice(prevDate))-1
+
+            return (round(dailyret, 4))
+
+        else:
+            # print('in else statement of daily ret')
+
+            date = datetime.strptime(date, "%Y-%m-%d")
+            prevDate = date - timedelta(days=1)
+            dailyret = (self.CurPrice(date)/self.CurPrice(prevDate))-1
+
+            return (round(dailyret, 4))
+
+    def Last30Dayprice(self, date=None):
+        if date is None:
+            # print('in if statement of last 30 days')
+
+            prevMonthDate = self.todays_date - timedelta(days=30)
+            return (self.hist_data.loc[prevMonthDate:self.todays_date]['Adj Close']).to_numpy()
+
+        else:
+            # print('in else statement of las 30 days')
+
+            date = datetime.strptime(date, "%Y-%m-%d")
+            prevMonthDate = date - timedelta(days=30)
+            return (self.hist_data.loc[prevMonthDate:date]['Adj Close']).to_numpy()
 
 
+# MAIN
 s1 = Stocks('ADANIPORTS.NS')
 
-s1.CurPrice('2024-02-14')
-s1.MonthlyRet()  # from to[will be 30 days before the start date]
-s1.DailyReturn()  # from to
-s1.Last30Dayprice()  # from to
+print(s1.CurPrice('2024-02-14'))
+print(s1.MonthlyRet())  # from to[will be 30 days before the start date]
+print(s1.DailyReturn())  # can take any date upto an year
+print(s1.Last30Dayprice())  # can take any date of the year
 
 
 nifty50_ticker_list = ['ADANIPORTS.NS', 'ASIANPAINT.NS', 'AXISBANK.NS', 'BAJAJ-AUTO.NS', 'BAJFINANCE.NS', 'BAJAJFINSV.NS', 'BPCL.NS', 'BHARTIARTL.NS', 'INFRATEL.NS', 'CIPLA.NS',
